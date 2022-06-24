b7d984d6110e640a80314d86feb6d0bccdb40005
damiony <damion_repo@163.com>
2022-06-24 23:22:37 +0800

    update

--- lua/dap/nvim-dap.lua
+++ lua/dap/nvim-dap.lua
@@ -1,5 +1,5 @@
 local dap = require "dap"
-vim.fn.sign_define('DapBreakpoint', {text='〽️', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpoint', {text='🎾', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapStopped', {text='🥤', texthl='Search', linehl='Visual', numhl='Visual'})
 
 require "dap/nvim-dap-go"
